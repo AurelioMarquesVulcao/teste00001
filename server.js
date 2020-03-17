2c4dc1c2b26a6f818962b17298d0003cab661edf
Aurelio Vulcão <vulcao@vulcaotech.com>
2020-03-17 14:21:52 -0300

    teste

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const app = require("./src/app");
 
-const port = process.env.Port || 8000;
+const port = process.env.Port || 3000;
 
 const server = http.createServer(app);
 server.listen(port);
